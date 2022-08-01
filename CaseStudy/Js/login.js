226b449e88e5f82d9e07408bda366d0ec2260898
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-08-01 11:55:45 +0700

    Case Study

--- CaseStudy/Js/login.js
+++ CaseStudy/Js/login.js
@@ -3,7 +3,7 @@ function checkAndNextPage() {
     let pass = document.getElementById("password").value;
 
     if (checkUse(use,pass)) {
-        location.replace("http://localhost:63342/Case%20Study/CaseStudy/CaseStudy.html?_ijt=p9sc9bcjsobltvdk9r879pr6ib");
+        location.replace("http://localhost:63342/Case%20Study/CaseStudy/html/CaseStudy.html?_ijt=16tblrfho323di5cao7rlu0ti2");
     }
     else {
         alert("Use hoặc Password đang sai! Hãy nhập lại")
